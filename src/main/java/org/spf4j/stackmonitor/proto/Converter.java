dd660ad56da6cebab939c7f846e97353573a4b8e
zolyfarkas@yahoo.com <zolyfarkas@yahoo.com@d408096a-aa36-d13b-a661-48d75a76e993>
2013-03-23 17:49:35 +0000

    [cleanup]
    git-svn-id: https://spf4j.googlecode.com/svn/trunk@138 d408096a-aa36-d13b-a661-48d75a76e993

--- src/main/java/org/spf4j/stackmonitor/proto/Converter.java
+++ src/main/java/org/spf4j/stackmonitor/proto/Converter.java
@@ -1,5 +1,19 @@
-/**
- * (c) Zoltan Farkas 2012
+/*
+ * Copyright (c) 2001, Zoltan Farkas All Rights Reserved.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
 package org.spf4j.stackmonitor.proto;
 
@@ -14,8 +28,9 @@ import java.util.Map;
  *
  * @author zoly
  */
-public class Converter {
+public final class Converter {
     
+    private Converter() {}
     
     public static ProtoSampleNodes.Method fromMethodToProto(Method m) {
         return ProtoSampleNodes.Method.newBuilder().setMethodName(m.getMethodName())
