ec60095d25b2e010ea509fbf18aa38cfebdafdb1
zolyfarkas@yahoo.com <zolyfarkas@yahoo.com@d408096a-aa36-d13b-a661-48d75a76e993>
2013-12-13 16:38:43 +0000

    [add] string distance calculators
    git-svn-id: https://spf4j.googlecode.com/svn/trunk@312 d408096a-aa36-d13b-a661-48d75a76e993

--- spf4j-core/src/main/java/org/spf4j/base/Strings.java
+++ spf4j-core/src/main/java/org/spf4j/base/Strings.java
@@ -6,6 +6,8 @@ package org.spf4j.base;
 
 import java.io.IOException;
 import java.io.Writer;
+import static java.lang.Math.min;
+import javax.annotation.Nonnull;
 
 /**
  *
@@ -97,6 +99,46 @@ public final class Strings {
             }
         }
         return i;
+    }
+    
+    /**
+     * function that calculates the number of operations that are needed to
+     * transform s1 into s2.
+     * operations are: char add, char delete, char modify
+     * @param s1
+     * @param s2
+     * @return the number of operations required to transfor s1 into s2
+     */
+    public static int distance(@Nonnull final String s1, @Nonnull final String s2) {
+        int l1 = s1.length();
+        int l2 = s2.length();
+
+        int[] prev = new int[l2];
+        char c1 = s1.charAt(0);
+        prev[0] = distance(c1, s2.charAt(0));
+        for (int j = 1; j < l2; j++) {
+            prev[j] = prev[j - 1] + distance(c1, s2.charAt(j));
+        }
+
+        for (int i = 1; i < l1; i++) {
+            int[] dist = new int[l2];
+            c1 = s1.charAt(i);
+            dist[0] = prev[i - 1] + distance(c1, s2.charAt(0));
+            for (int j = 1; j < l2; j++) {
+                dist[j] = min(prev[j - 1] + distance(c1, s2.charAt(j)),
+                        min(prev[j] + 1, dist[j - 1] + 1));
+            }
+            prev = dist;
+        }
+        return prev[l2 - 1];
+    }
+
+    public static int distance(final char c1, final char c2) {
+        if (c1 == c2) {
+            return 0;
+        } else {
+            return 1;
+        }
     }
     
     
