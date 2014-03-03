b040c730900461260f67ed555eb2dcda3c655e9f
zolyfarkas@yahoo.com <zolyfarkas@yahoo.com@d408096a-aa36-d13b-a661-48d75a76e993>
2014-03-03 04:21:52 +0000

    [add] more tests
    git-svn-id: https://spf4j.googlecode.com/svn/trunk@373 d408096a-aa36-d13b-a661-48d75a76e993

---
+++ spf4j-zel/src/test/java/org/spf4j/zel/vm/TestDoubleOperations.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2001, Zoltan Farkas All Rights Reserved.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+package org.spf4j.zel.vm;
+
+import junit.framework.Assert;
+import org.junit.Test;
+
+/**
+ *
+ * @author zoly
+ */
+public final  class TestDoubleOperations {
+    
+    
+    @Test
+    public void test() throws CompileException, ZExecutionException, InterruptedException {
+        Program prog = Program.compile("(1d+3d-2)/2*1");
+        Double result = (Double) prog.execute();
+        System.out.println(result.toString());
+        Assert.assertEquals(1, result.intValue());
+    }
+    
+    
+}
