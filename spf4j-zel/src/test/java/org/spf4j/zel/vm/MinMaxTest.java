56c4c085d67f3d491fe146161ed1a47de3d79f06
zolyfarkas@yahoo.com <zolyfarkas@yahoo.com@d408096a-aa36-d13b-a661-48d75a76e993>
2014-05-10 00:37:56 +0000

    [add] improve if statement + extra test
    git-svn-id: https://spf4j.googlecode.com/svn/trunk@411 d408096a-aa36-d13b-a661-48d75a76e993

---
+++ spf4j-zel/src/test/java/org/spf4j/zel/vm/MinMaxTest.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2001, Zoltan Farkas All Rights Reserved.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+package org.spf4j.zel.vm;
+
+import com.google.common.base.Charsets;
+import com.google.common.io.Resources;
+import java.io.IOException;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ *
+ * @author zoly
+ */
+public final class MinMaxTest {
+
+    @Test
+    public void test() throws CompileException, ZExecutionException, InterruptedException, IOException {
+        String qsort = Resources.toString(Resources.getResource(MinMaxTest.class, "minmax.zel"),
+                Charsets.US_ASCII);
+        Program p = Program.compile(qsort);
+        System.out.println(p);
+        Integer max = (Integer) p.execute();
+        Assert.assertEquals(8, (int) max);
+    }
+    
+    
+}
