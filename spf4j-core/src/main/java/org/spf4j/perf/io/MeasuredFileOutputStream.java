1cc502c69ad9e2a19ffddef9e8b62cfe994489ea
zolyfarkas@yahoo.com <zolyfarkas@yahoo.com@d408096a-aa36-d13b-a661-48d75a76e993>
2014-09-18 13:47:16 +0000

    [release]  cleanup, upgrade to latest findbugs
    git-svn-id: https://spf4j.googlecode.com/svn/trunk@497 d408096a-aa36-d13b-a661-48d75a76e993

--- spf4j-core/src/main/java/org/spf4j/perf/io/MeasuredFileOutputStream.java
+++ spf4j-core/src/main/java/org/spf4j/perf/io/MeasuredFileOutputStream.java
@@ -1,9 +1,25 @@
+
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright (c) 2001, Zoltan Farkas All Rights Reserved.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
 package org.spf4j.perf.io;
 
+import edu.umd.cs.findbugs.annotations.CleanupObligation;
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import java.io.File;
 import java.io.FileDescriptor;
 import java.io.FileNotFoundException;
@@ -15,6 +31,8 @@ import org.spf4j.perf.MeasurementRecorderSource;
  *
  * @author zoly
  */
+@CleanupObligation
+@SuppressFBWarnings("OBL_UNSATISFIED_OBLIGATION")
 public final class MeasuredFileOutputStream extends FileOutputStream {
 
     private final Class<?> from;
