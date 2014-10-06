37596a190b4e319ca3abb374049b174676ee8560
zolyfarkas@yahoo.com <zolyfarkas@yahoo.com@d408096a-aa36-d13b-a661-48d75a76e993>
2014-10-06 02:10:17 +0000

    [add] improvements on tailer.
    git-svn-id: https://spf4j.googlecode.com/svn/trunk@530 d408096a-aa36-d13b-a661-48d75a76e993

--- spf4j-core/src/test/java/org/spf4j/perf/tsdb/TSDBTailerTest.java
+++ spf4j-core/src/test/java/org/spf4j/perf/tsdb/TSDBTailerTest.java
@@ -5,28 +5,28 @@
 package org.spf4j.perf.tsdb;
 
 import java.io.File;
+import java.io.IOException;
+import java.lang.management.ManagementFactory;
 import java.util.Arrays;
 import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 import junit.framework.Assert;
 import org.junit.Test;
-import org.spf4j.base.AbstractRunnable;
 import org.spf4j.base.MutableHolder;
 import org.spf4j.concurrent.DefaultExecutor;
+import org.spf4j.concurrent.FileBasedLockTest;
 
 /**
  *
  * @author zoly
  */
 public final class TSDBTailerTest {
-    
-    public TSDBTailerTest() {
-    }
 
     private static final String FILE_NAME = System.getProperty("java.io.tmpdir") + "/testdb.tsdb";
 
     private volatile boolean finish = false;
-    
+
     /**
      * Test of close method, of class TimeSeriesDatabase.
      */
@@ -36,50 +36,31 @@ public final class TSDBTailerTest {
         if (new File(FILE_NAME).delete()) {
             System.out.println("existing tsdb file deleted");
         }
-        final TimeSeriesDatabase instance = new TimeSeriesDatabase(FILE_NAME, new byte[] {});
+        final TimeSeriesDatabase instance = new TimeSeriesDatabase(FILE_NAME, new byte[]{});
         Future<Integer> tailFut = DefaultExecutor.INSTANCE.submit(new Callable<Integer>() {
 
             @Override
             public Integer call() throws Exception {
-                final MutableHolder<Integer> counter = new MutableHolder<>(0);
-                instance.tail(1, 0, new TSDataHandler() {
-
-                    @Override
-                    public void newTable(final String tableName, final String[] columnNames) {
-                        System.out.println("New Table: " + tableName
-                                + " columns: " + Arrays.toString(columnNames));
-                    }
-
-                    @Override
-                    public void newData(final String tableName, final TimeSeries data) {
-                        System.out.println("Table " + tableName + " - " + data);
-                        counter.setValue(counter.getValue() + data.getTimeStamps().length);
-                    }
-
-                    @Override
-                    public boolean finish() {
-                        return finish;
-                    }
-                });
-                return counter.getValue();
+                return doTail(instance);
             }
+
         });
-        
-        instance.addTSTable("gr1", new byte []{}, 5, new String[]{"a", "b"}, new byte [][] {});
-        instance.write(System.currentTimeMillis(), "gr1", new long[] {0, 1});
+
+        instance.addTSTable("gr1", new byte[]{}, 5, new String[]{"a", "b"}, new byte[][]{});
+        instance.write(System.currentTimeMillis(), "gr1", new long[]{0, 1});
         Thread.sleep(5);
-        instance.write(System.currentTimeMillis(), "gr1", new long[] {1, 2});
+        instance.write(System.currentTimeMillis(), "gr1", new long[]{1, 2});
         Thread.sleep(5);
-        instance.write(System.currentTimeMillis(), "gr1", new long[] {3, 4});
+        instance.write(System.currentTimeMillis(), "gr1", new long[]{3, 4});
         Thread.sleep(5);
-        instance.addTSTable("gr2", new byte []{}, 5, new String[] {"a", "b"}, new byte [][] {});
-        instance.write(System.currentTimeMillis(), "gr2",  new long[] {7, 8});
+        instance.addTSTable("gr2", new byte[]{}, 5, new String[]{"a", "b"}, new byte[][]{});
+        instance.write(System.currentTimeMillis(), "gr2", new long[]{7, 8});
         instance.flush();
-        
-        instance.addTSTable("gr3", new byte []{}, 5, new String[] {"a", "b"}, new byte [][] {});
-        instance.write(System.currentTimeMillis(), "gr3",  new long[] {7, 8});
+
+        instance.addTSTable("gr3", new byte[]{}, 5, new String[]{"a", "b"}, new byte[][]{});
+        instance.write(System.currentTimeMillis(), "gr3", new long[]{7, 8});
         Thread.sleep(5);
-        instance.write(System.currentTimeMillis(), "gr3",  new long[] {9, 10});
+        instance.write(System.currentTimeMillis(), "gr3", new long[]{9, 10});
         instance.flush();
         Thread.sleep(1000);
         finish = true;
@@ -87,7 +68,76 @@ public final class TSDBTailerTest {
         Assert.assertEquals(6, result);
     }
 
+    @Test
+    public void externalTailerTest() throws InterruptedException, ExecutionException {
+        final String classPath = ManagementFactory.getRuntimeMXBean().getClassPath();
+        final String jvmPath
+                = System.getProperties().getProperty("java.home")
+                + File.separatorChar + "bin" + File.separatorChar + "java";
 
+        Future<Integer> result = DefaultExecutor.INSTANCE.submit(new Callable<Integer>() {
+            @Override
+            public Integer call() throws IOException, InterruptedException {
+                String[] command = new String[]{jvmPath, "-cp", classPath, TSDBTailerTest.class.getName(),
+                    FILE_NAME};
+                System.out.println("Running " + Arrays.toString(command));
+                Process proc = Runtime.getRuntime().exec(command);
+                return proc.waitFor();
+            }
+        });
+        Assert.assertEquals(6, (int) result.get());
 
+    }
+
+    private Integer doTail(final TimeSeriesDatabase instance) throws IOException {
+        final MutableHolder<Integer> counter = new MutableHolder<>(0);
+        instance.tail(1, 0, new TSDataHandler() {
+
+            @Override
+            public void newTable(final String tableName, final String[] columnNames) {
+                System.out.println("New Table: " + tableName
+                        + " columns: " + Arrays.toString(columnNames));
+            }
+
+            @Override
+            public void newData(final String tableName, final TimeSeries data) {
+                System.out.println("Table " + tableName + " - " + data);
+                counter.setValue(counter.getValue() + data.getTimeStamps().length);
+            }
+
+            @Override
+            public boolean finish() {
+                return finish;
+            }
+        });
+        return counter.getValue();
+    }
+
+    public static void main(final String[] parameters) throws IOException {
+        final MutableHolder<Integer> counter = new MutableHolder<>(0);
+        TimeSeriesDatabase tsdb = new TimeSeriesDatabase(parameters[0]);
+        tsdb.tail(1, 0, new TSDataHandler() {
+            
+            private int count = 0;
+
+            @Override
+            public void newTable(final String tableName, final String[] columnNames) {
+                System.out.println("New Table: " + tableName
+                        + " columns: " + Arrays.toString(columnNames));
+            }
+
+            @Override
+            public void newData(final String tableName, final TimeSeries data) {
+                System.out.println("Table " + tableName + " - " + data);
+                counter.setValue(counter.getValue() + data.getTimeStamps().length);
+            }
+
+            @Override
+            public boolean finish() {
+                return count++ > 2000;
+            }
+        });
+        System.exit(counter.getValue());
+    }
 
 }
