971ae705b28807ee1410c5c8b8ffc0154bec45ca
zolyfarkas@yahoo.com <zolyfarkas@yahoo.com@d408096a-aa36-d13b-a661-48d75a76e993>
2014-10-06 13:23:05 +0000

    [fix] cleanup tests
    git-svn-id: https://spf4j.googlecode.com/svn/trunk@533 d408096a-aa36-d13b-a661-48d75a76e993

--- spf4j-core/src/test/java/org/spf4j/perf/tsdb/TSDBTailerTest.java
+++ spf4j-core/src/test/java/org/spf4j/perf/tsdb/TSDBTailerTest.java
@@ -1,6 +1,19 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ /*
+ * Copyright (c) 2001, Zoltan Farkas All Rights Reserved.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
 package org.spf4j.perf.tsdb;
 
@@ -9,7 +22,6 @@ import java.io.IOException;
 import java.lang.management.ManagementFactory;
 import java.util.Arrays;
 import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 import junit.framework.Assert;
 import org.junit.Test;
