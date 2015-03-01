f73323b5f46c2b20b7d49e5de3eb31690e25bfc1
zolyfarkas@yahoo.com <zolyfarkas@yahoo.com@d408096a-aa36-d13b-a661-48d75a76e993>
2015-03-01 16:54:49 +0000

    [fix] eliminate benchmark from test
    git-svn-id: https://spf4j.googlecode.com/svn/trunk@664 d408096a-aa36-d13b-a661-48d75a76e993

--- spf4j-core/src/test/java/org/spf4j/base/ReflectionsTest.java
+++ spf4j-core/src/test/java/org/spf4j/base/ReflectionsTest.java
@@ -17,14 +17,9 @@
  */
 package org.spf4j.base;
 
-import com.google.common.cache.CacheBuilder;
-import com.google.common.cache.CacheLoader;
-import com.google.common.cache.LoadingCache;
 import java.lang.reflect.Method;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import junit.framework.Assert;
 import org.junit.Test;
-import static org.spf4j.base.Reflections.getCompatibleMethod;
 
 /**
  *
@@ -32,46 +27,12 @@ import static org.spf4j.base.Reflections.getCompatibleMethod;
  */
 public final class ReflectionsTest {
 
-    private static final LoadingCache<Reflections.MethodDesc, Method> CACHE = CacheBuilder.newBuilder()
-            .build(new CacheLoader<Reflections.MethodDesc, Method>() {
-                @Override
-                public Method load(final Reflections.MethodDesc k) throws Exception {
-                    return getCompatibleMethod(k.getClasz(), k.getName(), k.getParamTypes());
-                }
-            });
-
-    public static Method getCompatibleMethodCachedGuava(final Class<?> c,
-            final String methodName,
-            final Class<?>... paramTypes) {
-        return CACHE.getUnchecked(new Reflections.MethodDesc(c, methodName, paramTypes));
-    }
-
     @Test
-    public void testPerformance() {
+    public void testReflections() {
         Class<?>[] params = new Class<?>[]{String.class, Integer.class};
-        long startTimeS = System.currentTimeMillis();
-        Method reflect = null;
-        for (int i = 0; i < 1000000; i++) {
-            reflect = Reflections.getCompatibleMethod(String.class, "indexOf", params);
-        }
-        long startTime = System.currentTimeMillis();
-        Method fastM = null;
-        for (int i = 0; i < 10000000; i++) {
-            fastM = Reflections.getCompatibleMethodCached(String.class, "indexOf", params);
-        }
-        long startTimeG = System.currentTimeMillis();
-        Method guavaM = null;
-        for (int i = 0; i < 10000000; i++) {
-            guavaM = getCompatibleMethodCachedGuava(String.class, "indexOf", params);
-        }
-        long endTime = System.currentTimeMillis();
-        assertEquals(fastM, guavaM);
-        System.out.println("Guava Exec Time " + (endTime - startTimeG));
-        System.out.println("Fast Exec Time " + (startTimeG - startTime));
-        System.out.println("Uncached Reflection Exec Time " + (startTime- startTimeS)*10);
-
-        assertTrue("guava caching should be slower", (endTime - startTimeG) > (startTimeG - startTime));
-        assertTrue("uncached reflection should be slowest", (startTime- startTimeS)*10 > (endTime - startTimeG));
+        Method reflect = Reflections.getCompatibleMethod(String.class, "indexOf", params);
+        Method fastM = Reflections.getCompatibleMethodCached(String.class, "indexOf", params);
+        Assert.assertEquals(reflect, fastM);
     }
 
 }
