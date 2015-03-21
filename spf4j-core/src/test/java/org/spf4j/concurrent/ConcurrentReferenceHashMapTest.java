09fa1de88df0c4821f0c323057f16f2ebd4d6024
Zoltan Farkas <zolyfarkas@yahoo.com>
2015-03-21 17:41:35 -0400

    [cleanup][add] unit test to increase test coverage

---
+++ spf4j-core/src/test/java/org/spf4j/concurrent/ConcurrentReferenceHashMapTest.java
@@ -0,0 +1,33 @@
+
+package org.spf4j.concurrent;
+
+import java.util.Map;
+import junit.framework.Assert;
+import org.junit.Test;
+
+/**
+ *
+ * @author zoly
+ */
+public class ConcurrentReferenceHashMapTest {
+
+
+    @Test
+    public void testSomeMethod() {
+        ConcurrentReferenceHashMap<String, String> map = new ConcurrentReferenceHashMap();
+        for (int i = 0; i < 10000; i++) {
+            String key = "k" + i;
+            String value = "v" + i;
+            map.put(key, value);
+            Assert.assertEquals(value, map.get(key));
+        }
+        int size = map.size();
+        int count = 0;
+        for (Map.Entry<String, String> entry : map.entrySet()) {
+            count++;
+        }
+        Assert.assertEquals(size, count);
+
+    }
+
+}
