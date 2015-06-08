ad699fd4d0cf058b7a0833c62c9585d585e3d6ec
Zoltan Farkas <zolyfarkas@yahoo.com>
2015-06-08 10:25:01 -0400

    [add] another usage test

--- spf4j-core/src/test/java/org/spf4j/recyclable/impl/SharingObjectPoolTest.java
+++ spf4j-core/src/test/java/org/spf4j/recyclable/impl/SharingObjectPoolTest.java
@@ -17,8 +17,36 @@ public class SharingObjectPoolTest {
 
 
     @Test
-    public void testSharingPool() throws ObjectCreationException, ObjectBorrowException, InterruptedException, TimeoutException {
+    public void testSharingPool()
+            throws ObjectCreationException, ObjectBorrowException, InterruptedException, TimeoutException {
 
+        SharingObjectPool<String> pool = createTestPool();
+
+        String obj = pool.get();
+        Assert.assertEquals("O0", obj);
+        String obj2 = pool.get();
+        Assert.assertEquals("O1", obj2);
+        pool.recycle(obj);
+        String obj3 = pool.get();
+        Assert.assertEquals("O0", obj3);
+
+    }
+
+    @Test
+    public void testSharingPool2()
+            throws ObjectCreationException, ObjectBorrowException, InterruptedException, TimeoutException {
+
+        SharingObjectPool<String> pool = createTestPool();
+
+        String obj = pool.get();
+        Assert.assertEquals("O0", obj);
+        pool.recycle(obj);
+        String obj2 = pool.get();
+        Assert.assertEquals("O0", obj2);
+    }
+
+
+    public SharingObjectPool<String> createTestPool() throws ObjectCreationException {
         SharingObjectPool<String> pool = new SharingObjectPool<>(new RecyclingSupplier.Factory<String>(){
 
             int i = 0;
@@ -36,16 +64,8 @@ public class SharingObjectPoolTest {
             public boolean validate(String object, Exception e) throws Exception {
                 throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
             }
-        }, 0, 2);
-
-        String obj = pool.get();
-        Assert.assertEquals("O0", obj);
-        String obj2 = pool.get();
-        Assert.assertEquals("O1", obj2);
-        pool.recycle(obj);
-        String obj3 = pool.get();
-        Assert.assertEquals("O0", obj3);
-
+        }, 0, 4);
+        return pool;
     }
 
 }
