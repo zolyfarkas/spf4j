5bb416a7c9d2405d99f9ec2f2272edecfcd6791b
Zoltan Farkas <zolyfarkas@yahoo.com>
2015-07-07 16:40:01 -0400

    [add] enhancements to retry utilities.

--- spf4j-aspects/src/main/java/org/spf4j/annotations/VoidPredicate.java
+++ spf4j-aspects/src/main/java/org/spf4j/annotations/VoidPredicate.java
@@ -3,7 +3,7 @@ package org.spf4j.annotations;
 
 import org.spf4j.base.Callables;
 
-public final class VoidPredicate implements Callables.AdvancedRetryPredicate<Exception> {
+public final class VoidPredicate extends Callables.AdvancedRetryPredicate<Exception> {
 
     @Override
     public Callables.AdvancedAction apply(final Exception value) {
