cee8e3a1b2a77b3ef4ad76fbfca08274acf8fe54
Zoltan Farkas <zolyfarkas@yahoo.com>
2016-04-07 12:35:58 -0400

    [add] more safe implementation in case appendable is closed.

--- spf4j-core/src/main/java/org/spf4j/io/AppendableWriter.java
+++ spf4j-core/src/main/java/org/spf4j/io/AppendableWriter.java
@@ -18,60 +18,110 @@ public final class AppendableWriter extends Writer {
   private final Appendable appendable;
   
   private final boolean flushable;
+  
+  private boolean closed;
 
   public AppendableWriter(final Appendable appendable) {
     this.appendable = appendable;
     this.flushable = appendable instanceof Flushable;
+    this.closed = false;
   }
 
   @Override
   public void write(final char[] cbuf, final int off, final int len) throws IOException {
+    checkNotClosed();
+    /* Guava claims:
+    It turns out that creating a new String is usually as fast,
+    or faster than wrapping cbuf in a light-weight CharSequence.
+    
+    Since I am suspicious of claims that do not make much sense and most developers write rubbish benchmarks
+    (Linus I start to be like you more and more),
+    I wrote a JMH benchmark, and the results are as expected the opposite:
+    
+    Benchmark                                          Mode  Cnt      Score     Error  Units
+    AppendableWriterBenchmark.guavaAppendable         thrpt   10  10731.940 ± 427.258  ops/s
+    AppendableWriterBenchmark.spf4jAppendable         thrpt   10  17613.093 ± 344.769  ops/s
+     
+     Using a light weight wrapper is more than 50% faster!
+    
+     See AppendableWriterBenchmark for more detail...
+    
+    */
     appendable.append(CharBuffer.wrap(cbuf), off, off + len);
   }
 
   @Override
   public void write(final int c) throws IOException {
+    checkNotClosed();
     appendable.append((char) c);
   }
 
   @Override
   public Writer append(final char c) throws IOException {
+    checkNotClosed();
     appendable.append(c);
     return this;
   }
 
   @Override
   public Writer append(final CharSequence csq, final int start, final int end) throws IOException {
-      appendable.append(csq, start, end);
-      return this;
+    checkNotClosed();
+    appendable.append(csq, start, end);
+    return this;
   }
 
   @Override
   public Writer append(final CharSequence csq) throws IOException {
+    checkNotClosed();
     appendable.append(csq);
     return this;
   }
 
+  @Override
+  public void write(final String str, final int off, final int len) throws IOException {
+    checkNotClosed();
+    appendable.append(str, off, off + len);
+  }
+
+  @Override
+  public void write(final String str) throws IOException {
+    appendable.append(str);
+  }
+
+  @Override
+  public void write(final char[] cbuf) throws IOException {
+    appendable.append(CharBuffer.wrap(cbuf));
+  }
 
   @Override
   public void flush() throws IOException {
+    checkNotClosed();
     if (flushable) {
       ((Flushable) appendable).flush();
     }
   }
 
+  private void checkNotClosed() throws IOException {
+    if (closed) {
+      throw new IOException("Cannot write to closed writer " + this);
+    }
+  }
+  
   @Override
   @DischargesObligation
   public void close() throws IOException {
-    flush();
-    if (appendable instanceof Closeable) {
-      ((Closeable) appendable).close();
+    if (!closed) {
+      flush();
+      if (appendable instanceof Closeable) {
+        ((Closeable) appendable).close();
+      }
+      closed = true;
     }
   }
 
   @Override
   public String toString() {
-    return "AppendableWriter{" + "appendable=" + appendable + '}';
+    return "AppendableWriter{" + "appendable=" + appendable + ", flushable=" + flushable + ", closed=" + closed + '}';
   }
 
 }
