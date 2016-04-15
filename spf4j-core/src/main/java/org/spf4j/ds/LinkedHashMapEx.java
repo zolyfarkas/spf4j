63c6a6e2f84c87c304e71e40f9e9acbe70068f0a
Zoltan Farkas <zolyfarkas@yahoo.com>
2016-04-14 22:33:13 -0400

    [fix] Implementation to work with JDK 1.8.

--- spf4j-core/src/main/java/org/spf4j/ds/LinkedHashMapEx.java
+++ spf4j-core/src/main/java/org/spf4j/ds/LinkedHashMapEx.java
@@ -1,4 +1,3 @@
-
 package org.spf4j.ds;
 
 import java.lang.reflect.Field;
@@ -13,120 +12,70 @@ import javax.annotation.Nullable;
  * @author zoly
  */
 public final class LinkedHashMapEx<K, V> extends LinkedHashMap<K, V>
-    implements LinkedMap<K, V> {
+        implements LinkedMap<K, V> {
 
-    private static final Field HEADER;
-    private static final Field HEADER_BEFORE;
-    private static final long serialVersionUID = 1L;
+  private static final Field TAIL;
+  private static final long serialVersionUID = 1L;
 
-    static {
-        HEADER = AccessController.doPrivileged(new PrivilegedAction<Field>() {
-            @Override
-            public Field run() {
-                try {
-                    Field field = LinkedHashMap.class.getDeclaredField("header");
-                    field.setAccessible(true);
-                    return field;
-                } catch (NoSuchFieldException | SecurityException ex) {
-                    throw new RuntimeException(ex);
-                }
-            }
-        });
-        HEADER_BEFORE = AccessController.doPrivileged(new PrivilegedAction<Field>() {
-            @Override
-            public Field run() {
-                try {
-                    Class<?> entryClass = Class.forName("java.util.LinkedHashMap$Entry");
-                    Field field = entryClass.getDeclaredField("before");
-                    field.setAccessible(true);
-                    return field;
-                } catch (NoSuchFieldException | SecurityException | ClassNotFoundException ex) {
-                    throw new RuntimeException(ex);
-                }
-            }
-        });
+  static {
 
-    }
-
-    private Map.Entry<K, V> getHeader() {
+    TAIL = AccessController.doPrivileged(new PrivilegedAction<Field>() {
+      @Override
+      public Field run() {
         try {
-            return (Map.Entry<K, V>) HEADER.get(this);
-        } catch (IllegalArgumentException | IllegalAccessException ex) {
-            throw new RuntimeException(ex);
+          Field field = LinkedHashMap.class.getDeclaredField("tail");
+          field.setAccessible(true);
+          return field;
+        } catch (NoSuchFieldException | SecurityException ex) {
+          throw new RuntimeException(ex);
         }
+      }
+    });
+
+  }
+
+  private Map.Entry<K, V> getTail() {
+    try {
+      return (Map.Entry<K, V>) TAIL.get(this);
+    } catch (IllegalArgumentException | IllegalAccessException ex) {
+      throw new RuntimeException(ex);
     }
+  }
 
-    public LinkedHashMapEx() {
-        super();
-        header = getHeader();
+  public LinkedHashMapEx() {
+    super();
+  }
+
+  public LinkedHashMapEx(final int initialCapacity, final float loadFactor) {
+    super(initialCapacity, loadFactor);
+  }
+
+  public LinkedHashMapEx(final int initialCapacity) {
+    super(initialCapacity);
+  }
+
+  public LinkedHashMapEx(final Map<? extends K, ? extends V> m) {
+    super(m);
+  }
+
+  public LinkedHashMapEx(final int initialCapacity, final float loadFactor, final boolean accessOrder) {
+    super(initialCapacity, loadFactor, accessOrder);
+  }
+
+  @Nullable
+  @Override
+  public Map.Entry<K, V> getLastEntry() {
+    return getTail();
+  }
+
+  @Nullable
+  @Override
+  public Map.Entry<K, V> pollLastEntry() {
+    final Entry<K, V> lastEntry = getTail();
+    if (lastEntry != null) {
+      remove(lastEntry.getKey());
     }
-
-
-    public LinkedHashMapEx(final int initialCapacity, final float loadFactor) {
-        super(initialCapacity, loadFactor);
-        header = getHeader();
-    }
-
-    public LinkedHashMapEx(final int initialCapacity) {
-        super(initialCapacity);
-        header = getHeader();
-    }
-
-
-
-    public LinkedHashMapEx(final Map<? extends K, ? extends V> m) {
-        super(m);
-        header = getHeader();
-    }
-
-    public LinkedHashMapEx(final int initialCapacity, final float loadFactor, final boolean accessOrder) {
-        super(initialCapacity, loadFactor, accessOrder);
-        header = getHeader();
-    }
-
-
-   private transient Map.Entry<K, V> header;
-
-   private void readObject(final java.io.ObjectInputStream s)
-        throws java.io.IOException, ClassNotFoundException {
-        s.defaultReadObject();
-        header = getHeader();
-   }
-
-
-    private Map.Entry<K, V> getLastEntryInternal() {
-        try {
-            return (Map.Entry<K, V>) HEADER_BEFORE.get(header);
-        } catch (IllegalArgumentException | IllegalAccessException ex) {
-            throw new RuntimeException(ex);
-        }
-    }
-
-
-
-
-   @Nullable
-    @Override
-    public Map.Entry<K, V> getLastEntry() {
-        final Entry<K, V> lastEntry = getLastEntryInternal();
-        if (lastEntry == header) {
-            return null;
-        } else {
-            return lastEntry;
-        }
-    }
-
-    @Nullable
-    @Override
-    public Map.Entry<K, V> pollLastEntry() {
-        final Entry<K, V> lastEntry = getLastEntryInternal();
-        if (lastEntry == header) {
-            return null;
-        } else {
-            remove(lastEntry.getKey());
-            return lastEntry;
-        }
-    }
-
+    return lastEntry;
+  }
 
 }
