df4643e371bd432b22f2f4f22a9b7714a874453d
Zoltan Farkas <zolyfarkas@yahoo.com>
2016-04-14 22:34:03 -0400

    [experimental] switch default executor to FJP

--- spf4j-core/src/main/java/org/spf4j/concurrent/DefaultExecutor.java
+++ spf4j-core/src/main/java/org/spf4j/concurrent/DefaultExecutor.java
@@ -20,14 +20,14 @@ package org.spf4j.concurrent;
 import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
-import java.util.concurrent.SynchronousQueue;
-import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.TimeUnit;
 import org.spf4j.base.AbstractRunnable;
 import static org.spf4j.base.Runtime.WAIT_FOR_SHUTDOWN_MILLIS;
 
 /**
- *
+ * This executor aims to be a general purpose executor for async tasks.
+ * (equivalent to ForkJoinPool.commonPool())
  * @author zoly
  */
 @SuppressFBWarnings("HES_EXECUTOR_NEVER_SHUTDOWN") // THere is a shutdownhook being registered which FB does not see
@@ -42,15 +42,16 @@ public final class DefaultExecutor {
         final int coreThreads = Integer.getInteger("defaultExecutor.coreThreads", 0);
         final int maxIdleMillis = Integer.getInteger("defaultExecutor.maxIdleMillis", 60000);
         final boolean isDaemon = Boolean.getBoolean("defaultExecutor.daemon");
-        if ("spf4j".equalsIgnoreCase(System.getProperty("defaultExecutor.implementation", "spf4j"))) {
+        if ("spf4j".equalsIgnoreCase(System.getProperty("defaultExecutor.implementation", "jdk"))) {
             LifoThreadPoolExecutorSQP lifoExec = new LifoThreadPoolExecutorSQP("defaultExecutor", coreThreads,
                     Integer.MAX_VALUE, maxIdleMillis, 0, isDaemon, Integer.getInteger("defaultExecutor.spinlockCount",
                             1024));
             lifoExec.exportJmx();
             INSTANCE = lifoExec;
         } else {
-            INSTANCE = new ThreadPoolExecutor(coreThreads, Integer.MAX_VALUE, maxIdleMillis, TimeUnit.MILLISECONDS,
-                new SynchronousQueue<Runnable>(), new CustomThreadFactory("DefaultExecutor", isDaemon));
+          INSTANCE = new ForkJoinPool(32767);
+//            INSTANCE = new ThreadPoolExecutor(coreThreads, Integer.MAX_VALUE, maxIdleMillis, TimeUnit.MILLISECONDS,
+//                new SynchronousQueue<Runnable>(), new CustomThreadFactory("DefaultExecutor", isDaemon));
         }
         org.spf4j.base.Runtime.queueHookAtEnd(new AbstractRunnable(true) {
 
