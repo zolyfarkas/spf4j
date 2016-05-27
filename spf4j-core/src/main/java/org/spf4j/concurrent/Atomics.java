aee7c47b1c12a9969fcd741ad35f3eced8b686b9
Zoltan Farkas <zolyfarkas@yahoo.com>
2016-05-27 11:56:40 -0400

    [fix] potential NPE.

--- spf4j-core/src/main/java/org/spf4j/concurrent/Atomics.java
+++ spf4j-core/src/main/java/org/spf4j/concurrent/Atomics.java
@@ -1,12 +1,15 @@
 package org.spf4j.concurrent;
 
+import java.util.Objects;
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.function.UnaryOperator;
+import javax.annotation.ParametersAreNonnullByDefault;
 
 /**
  *
  * @author zoly
  */
+@ParametersAreNonnullByDefault
 public final class Atomics {
 
   private Atomics() {
@@ -18,7 +21,7 @@ public final class Atomics {
     do {
       initial = ar.get();
       newObj = function.apply(initial);
-      if (initial.equals(newObj)) {
+      if (Objects.equals(initial, newObj)) {
         return UpdateResult.same(initial);
       } else if (initial == newObj) {
         throw new IllegalStateException("Function " + function + " is mutating " + initial + ", this is not allowed");
