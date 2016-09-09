6828adcd88dd595cb1f458a19f598c09bcec00b3
Zoltan Farkas <zolyfarkas@yahoo.com>
2016-09-09 17:47:52 -0400

    [add] experimental code to create a generic record in a "better" way

--- spf4j-experimental/src/main/java/org/spf4j/beans/GenericRecordBuilder.java
+++ spf4j-experimental/src/main/java/org/spf4j/beans/GenericRecordBuilder.java
@@ -5,6 +5,7 @@ import java.io.File;
 import org.apache.avro.Schema;
 import org.apache.avro.generic.GenericRecord;
 import org.apache.avro.compiler.specific.SpecificCompiler;
+import org.apache.avro.generic.GenericData;
 import org.codehaus.commons.compiler.AbstractJavaSourceClassLoader;
 import org.codehaus.commons.compiler.CompilerFactoryFactory;
 
@@ -18,10 +19,13 @@ public final class GenericRecordBuilder {
 
   public static Class<? extends GenericRecord> createClass(final Schema schema) throws Exception {
     SpecificCompiler sc = new SpecificCompiler(schema);
+    sc.setStringType(GenericData.StringType.String);
+    sc.setTemplateDir("org/spf4j/beans/");
     File tmp = Files.createTempDir();
     sc.compileToDestination(null, tmp);
     AbstractJavaSourceClassLoader source
-            = CompilerFactoryFactory.getDefaultCompilerFactory().newJavaSourceClassLoader();
+            = CompilerFactoryFactory.getDefaultCompilerFactory()
+                    .newJavaSourceClassLoader(Thread.currentThread().getContextClassLoader());
     source.setSourcePath(new File[]{tmp});
     return (Class<? extends GenericRecord>) source.loadClass(schema.getFullName());
   }
