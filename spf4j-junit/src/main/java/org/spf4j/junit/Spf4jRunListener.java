f2204c024c13042584bd160ec312ce897d8a9612
Zoltan Farkas <zolyfarkas@yahoo.com>
2016-09-13 12:28:00 -0400

    [add] javadoc

--- spf4j-junit/src/main/java/org/spf4j/junit/Spf4jRunListener.java
+++ spf4j-junit/src/main/java/org/spf4j/junit/Spf4jRunListener.java
@@ -11,6 +11,37 @@ import org.spf4j.stackmonitor.FastStackCollector;
 import org.spf4j.stackmonitor.Sampler;
 
 /**
+ * A Junit run listener that will collect performance profiles for your unit tests.
+ *
+ * Works only if unit tests are NOT executed in parallel inside the same JVM.
+ *
+ * To enable this Run listener with maven, you will need to configure your maven-surefire-plugin with:
+ *
+ *       <plugin>
+ *       <groupId>org.apache.maven.plugins</groupId>
+ *       <artifactId>maven-surefire-plugin</artifactId>
+ *       <configuration>
+ *         <properties>
+ *           <property>
+ *             <name>listener</name>
+ *             <value>org.spf4j.junit.Spf4jRunListener</value>
+ *           </property>
+ *           <property>
+ *             <name>spf4j.junit.sampleTimeMillis</name>
+ *             <value>5</value>
+ *           </property>
+ *         </properties>
+ *       </configuration>
+ *     </plugin>
+ *
+ * and add spf4j-junit to your test classpath:
+ *
+ *   <dependency>
+ *     <groupId>org.spf4j</groupId>
+ *     <artifactId>spf4j-junit</artifactId>
+ *     <version>${spf4j.version}</version>
+ *     <scope>test</scope>
+ *   </dependency>
  *
  * @author zoly
  */
