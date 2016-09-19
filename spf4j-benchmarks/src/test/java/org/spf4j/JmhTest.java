b6043fa97b3eab4eb6b25c5dde32eb841735e1f4
Zoltan Farkas <zolyfarkas@yahoo.com>
2016-09-19 12:23:14 -0400

    [disable] benchmarks in travis.

--- spf4j-benchmarks/src/test/java/org/spf4j/JmhTest.java
+++ spf4j-benchmarks/src/test/java/org/spf4j/JmhTest.java
@@ -1,8 +1,6 @@
-
 package org.spf4j;
 
 import java.io.IOException;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.openjdk.jmh.results.format.ResultFormatType;
 import org.openjdk.jmh.runner.Runner;
@@ -16,38 +14,40 @@ import org.spf4j.stackmonitor.Spf4jJmhProfiler;
  *
  * @author zoly
  */
-//@Ignore
 public final class JmhTest {
 
-    @Test
-    public void runJmh() throws RunnerException, IOException {
-        final String destinationFolder = System.getProperty("basedir",
-                org.spf4j.base.Runtime.USER_DIR) + "/target";
-        final String profile = System.getProperty("basedir",
-                org.spf4j.base.Runtime.USER_DIR) + "/src/main/jfc/profile.jfc";
-        Options opt = new OptionsBuilder()
-//                .include(".*Cache.*")
-//                .include(".*")
-//                .addProfiler(JmhProfiler.class)
-//                .addProfiler(CompilerProfiler.class)
-                .addProfiler(JmhFlightRecorderProfiler.class)
-                .addProfiler(Spf4jJmhProfiler.class)
-//                .addProfiler(GCProfiler.class)
-                //"-XX:+PrintCompilation", "-XX:+UseG1GC", "-XX:MinTLABSize=1m", "-XX:MaxInlineLevel=12"
-                // "-XX:+PrintInlining", "-XX:+TraceDeoptimization", "-XX:+DebugDeoptimization", "-XX:+LogEvents"
-                //"-XX:+UnlockDiagnosticVMOptions", "-XX:+LogEvents", "-XX:+PrintCodeCache", "-XX:MaxInlineLevel=12",
-                //         "-XX:+PrintInlining", "-XX:+PrintCompilation", "-XX:+LogCompilation"
-                .jvmArgs("-XX:MaxInlineLevel=12","-Xmx256m", "-Xms256m", "-XX:+UnlockCommercialFeatures",
-                        "-Djmh.stack.profiles=" + destinationFolder,
-                        "-Dspf4j.executors.defaultExecutor.daemon=true", "-Djmh.executor=FJP",
-                        "-Djmh.fr.options=defaultrecording=true,settings=" + profile)
-                .result(destinationFolder + "/" + "benchmarkResults.csv")
-                .resultFormat(ResultFormatType.CSV)
-                .warmupIterations(10)
-                .measurementIterations(10)
-                .forks(1)
-                .build();
-         new Runner(opt).run();
+  @Test
+  public void runJmh() throws RunnerException, IOException {
+    if ("true".equalsIgnoreCase(System.getenv("TRAVIS"))) {
+      System.err.println("Benchmarks disabled in travis, not enough resources for this...");
     }
+    final String destinationFolder = System.getProperty("basedir",
+            org.spf4j.base.Runtime.USER_DIR) + "/target";
+    final String profile = System.getProperty("basedir",
+            org.spf4j.base.Runtime.USER_DIR) + "/src/main/jfc/profile.jfc";
+    Options opt = new OptionsBuilder()
+            .include(".*GenericRecord.*")
+            //                .include(".*")
+            //                .addProfiler(JmhProfiler.class)
+            //                .addProfiler(CompilerProfiler.class)
+            .addProfiler(JmhFlightRecorderProfiler.class)
+            .addProfiler(Spf4jJmhProfiler.class)
+            //                .addProfiler(GCProfiler.class)
+            //"-XX:+PrintCompilation", "-XX:+UseG1GC", "-XX:MinTLABSize=1m", "-XX:MaxInlineLevel=12"
+            // "-XX:+PrintInlining", "-XX:+TraceDeoptimization", "-XX:+DebugDeoptimization", "-XX:+LogEvents"
+            //"-XX:+UnlockDiagnosticVMOptions", "-XX:+LogEvents", "-XX:+PrintCodeCache", "-XX:MaxInlineLevel=12",
+            //         "-XX:+PrintInlining", "-XX:+PrintCompilation", "-XX:+LogCompilation"
+            .jvmArgs("-XX:MaxInlineLevel=12", "-Xmx256m", "-Xms256m", "-XX:+UnlockCommercialFeatures",
+                    "-Djmh.stack.profiles=" + destinationFolder,
+                    "-Dspf4j.executors.defaultExecutor.daemon=true", "-Djmh.executor=FJP",
+                    "-Djmh.fr.options=defaultrecording=true,settings=" + profile)
+            .result(destinationFolder + "/" + "benchmarkResults.csv")
+            .resultFormat(ResultFormatType.CSV)
+            .warmupIterations(10)
+            .measurementIterations(10)
+            .forks(1)
+            .build();
+    new Runner(opt).run();
+  }
 
 }
