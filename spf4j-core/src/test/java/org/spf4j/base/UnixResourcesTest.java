f9fe572fada0f6438516a075c984aefc0a8fac14
Zoltan Farkas <zolyfarkas@yahoo.com>
2017-05-08 09:45:37 -0400

    [fix] disable unit test on Linux

--- spf4j-core/src/test/java/org/spf4j/base/UnixResourcesTest.java
+++ spf4j-core/src/test/java/org/spf4j/base/UnixResourcesTest.java
@@ -3,6 +3,7 @@ package org.spf4j.base;
 
 import java.io.IOException;
 import org.junit.Assert;
+import org.junit.Assume;
 import org.junit.Test;
 import static org.spf4j.base.Runtime.Ulimit.runUlimit;
 
@@ -14,10 +15,10 @@ public class UnixResourcesTest {
 
   @Test
   public void testUlimit() throws IOException {
+    Assume.assumeTrue(Runtime.isMacOsx());
     long softLimit = UnixResources.RLIMIT_NOFILE.getSoftLimit();
     Assert.assertEquals(softLimit,
             (long) runUlimit("-Sn"));
-
 //    UnixResources.RLIMIT_NOFILE.setSoftLimit(softLimit + 1);
 //    Assert.assertEquals(softLimit + 1, UnixResources.RLIMIT_NOFILE.getSoftLimit());
   }
