5d08f7ee460db3017e3836bcd22949b910fedd02
Zoltan Farkas <zolyfarkas@yahoo.com>
2017-05-26 14:00:39 -0400

    [cleanup]

--- spf4j-core/src/test/java/org/spf4j/io/CsvTest.java
+++ spf4j-core/src/test/java/org/spf4j/io/CsvTest.java
@@ -24,7 +24,6 @@ import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
