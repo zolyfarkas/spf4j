8acad529937bddec68daf4726c6170295f326187
Zoltan Farkas <zolyfarkas@yahoo.com>
2017-06-12 16:53:09 -0400

    [cleanup]

--- spf4j-core/src/test/java/org/spf4j/text/FastMessageFormatTest.java
+++ spf4j-core/src/test/java/org/spf4j/text/FastMessageFormatTest.java
@@ -1,7 +1,6 @@
 
 package org.spf4j.text;
 
-import org.spf4j.text.MessageFormat;
 import java.io.IOException;
 import org.junit.Assert;
 import org.junit.Test;
@@ -11,7 +10,7 @@ import org.junit.Test;
  * @author zoly
  */
 public class FastMessageFormatTest {
-  
+
 
 
   @Test
@@ -21,23 +20,23 @@ public class FastMessageFormatTest {
     format.format(new Object [] {"a", "b"}, sb, null);
     Assert.assertEquals("a, b", sb.toString());
   }
-  
+
   @Test
   public void testFormatter5() throws IOException {
     StringBuilder sb = new StringBuilder();
     MessageFormat format = new MessageFormat("{0}, {1}, {2}, {3}, {4}");
     format.format(new Object [] {"a", "b", "c", "d", "e"}, sb, null);
     Assert.assertEquals("a, b, c, d, e", sb.toString());
-  }  
-  
+  }
+
   @Test
   public void testFormatter2() throws IOException {
     StringBuilder sb = new StringBuilder();
     MessageFormat format = new MessageFormat("bla bla bla");
     format.format(null, sb, null);
     Assert.assertEquals("bla bla bla", sb.toString());
-  }  
-  
+  }
+
   @Test
   public void testFormatter3() throws IOException {
     StringBuilder sb = new StringBuilder();
@@ -45,14 +44,14 @@ public class FastMessageFormatTest {
     format.format(new Object [] {"a", "b"}, sb, null);
     Assert.assertEquals("pre b, a suf", sb.toString());
   }
-  
+
   @Test
   public void testFormatter4() throws IOException {
     StringBuilder sb = new StringBuilder();
     MessageFormat format = new MessageFormat("pre {1}, {0}, {2,number,$'#',##} suf");
     format.format(new Object [] {"a", "b", 100}, sb, null);
     Assert.assertEquals("pre b, a, $#1,00 suf", sb.toString());
-  }    
-  
- 
+  }
+
+
 }
