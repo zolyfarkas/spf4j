63b64b83ed754d0468cd823956d1d42ff9eae157
Zoltan Farkas <zolyfarkas@yahoo.com>
2017-06-14 23:20:38 -0400

    [cleanup]

--- spf4j-core/src/main/java/org/spf4j/base/Arrays.java
+++ spf4j-core/src/main/java/org/spf4j/base/Arrays.java
@@ -87,43 +87,8 @@ public final class Arrays {
     if (a1 == null || a2 == null) {
       return false;
     }
-
     for (int i = starting; i < ending; i++) {
-      Object e1 = a1[i];
-      Object e2 = a2[i];
-
-      if (e1 == e2) {
-        continue;
-      }
-      if (e1 == null) {
-        return false;
-      }
-
-      // Figure out whether the two elements are equal
-      boolean eq;
-      if (e1 instanceof Object[] && e2 instanceof Object[]) {
-        eq = java.util.Arrays.deepEquals((Object[]) e1, (Object[]) e2);
-      } else if (e1 instanceof byte[] && e2 instanceof byte[]) {
-        eq = java.util.Arrays.equals((byte[]) e1, (byte[]) e2);
-      } else if (e1 instanceof short[] && e2 instanceof short[]) {
-        eq = java.util.Arrays.equals((short[]) e1, (short[]) e2);
-      } else if (e1 instanceof int[] && e2 instanceof int[]) {
-        eq = java.util.Arrays.equals((int[]) e1, (int[]) e2);
-      } else if (e1 instanceof long[] && e2 instanceof long[]) {
-        eq = java.util.Arrays.equals((long[]) e1, (long[]) e2);
-      } else if (e1 instanceof char[] && e2 instanceof char[]) {
-        eq = java.util.Arrays.equals((char[]) e1, (char[]) e2);
-      } else if (e1 instanceof float[] && e2 instanceof float[]) {
-        eq = java.util.Arrays.equals((float[]) e1, (float[]) e2);
-      } else if (e1 instanceof double[] && e2 instanceof double[]) {
-        eq = java.util.Arrays.equals((double[]) e1, (double[]) e2);
-      } else if (e1 instanceof boolean[] && e2 instanceof boolean[]) {
-        eq = java.util.Arrays.equals((boolean[]) e1, (boolean[]) e2);
-      } else {
-        eq = e1.equals(e2);
-      }
-
-      if (!eq) {
+      if (!java.util.Objects.deepEquals(a1[i], a2[i])) {
         return false;
       }
     }
