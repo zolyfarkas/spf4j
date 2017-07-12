cc55478f43e80cef15c7e0cca203e6f8fe855f13
Zoltan Farkas <zolyfarkas@yahoo.com>
2017-07-11 22:40:50 -0400

    [add] generate better enums

--- spf4j-benchmarks/src/test/java/org/spf4j/avro/GenericRecordBenchmark.java
+++ spf4j-benchmarks/src/test/java/org/spf4j/avro/GenericRecordBenchmark.java
@@ -32,7 +32,7 @@ public class GenericRecordBenchmark {
     } catch (IOException ex) {
       throw new ExceptionInInitializerError(ex);
     }
-    CLASZ = new GenericRecordBuilder(SCHEMA).getClass(SCHEMA);
+    CLASZ = new GenericRecordBuilder(SCHEMA).getRecordClass(SCHEMA);
   }
 
 
