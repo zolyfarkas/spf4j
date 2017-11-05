e2857ceed7884861d7a9a995c48603bdece86fc1
Zoltan Farkas <zolyfarkas@yahoo.com>
2017-11-05 14:10:49 -0500

    [cleanup]

--- spf4j-core/src/main/java/org/spf4j/io/AppendableWriter.java
+++ spf4j-core/src/main/java/org/spf4j/io/AppendableWriter.java
@@ -31,8 +31,6 @@
  */
 package org.spf4j.io;
 
-import edu.umd.cs.findbugs.annotations.CleanupObligation;
-import edu.umd.cs.findbugs.annotations.DischargesObligation;
 import java.io.Closeable;
 import java.io.Flushable;
 import java.io.IOException;
@@ -44,13 +42,12 @@ import java.nio.CharBuffer;
  * this is a faster version of guava: CharStreams.asWriter
  * @author zoly
  */
-@CleanupObligation
 public final class AppendableWriter extends Writer {
 
   private final Appendable appendable;
-  
+
   private final boolean flushable;
-  
+
   private boolean closed;
 
   public AppendableWriter(final Appendable appendable) {
@@ -65,19 +62,19 @@ public final class AppendableWriter extends Writer {
     /* Guava claims:
     It turns out that creating a new String is usually as fast,
     or faster than wrapping cbuf in a light-weight CharSequence.
-    
+
     Since I am suspicious of claims that do not make much sense and most developers write rubbish benchmarks
     (Linus I start to be like you more and more),
     I wrote a JMH benchmark, and the results are as expected the opposite:
-    
+
     Benchmark                                          Mode  Cnt      Score     Error  Units
     AppendableWriterBenchmark.guavaAppendable         thrpt   10  10731.940 ± 427.258  ops/s
     AppendableWriterBenchmark.spf4jAppendable         thrpt   10  17613.093 ± 344.769  ops/s
-     
+
      Using a light weight wrapper is more than 50% faster!
-    
+
      See AppendableWriterBenchmark for more detail...
-    
+
     */
     appendable.append(CharBuffer.wrap(cbuf), off, off + len);
   }
@@ -138,9 +135,8 @@ public final class AppendableWriter extends Writer {
       throw new IOException("Cannot write to closed writer " + this);
     }
   }
-  
+
   @Override
-  @DischargesObligation
   public void close() throws IOException {
     if (!closed) {
       flush();
