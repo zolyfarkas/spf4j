7d77746945b814d2e2c9b476406ad579c7993cb0
Zoltan Farkas <zolyfarkas@yahoo.com>
2018-01-28 11:52:13 -0500

    [add] upgrade plugins.

--- spf4j-benchmarks/src/test/java/org/spf4j/JmhTest.java
+++ spf4j-benchmarks/src/test/java/org/spf4j/JmhTest.java
@@ -61,7 +61,7 @@ public final class JmhTest {
     final String profile = System.getProperty("basedir",
             org.spf4j.base.Runtime.USER_DIR) + "/src/main/jfc/profile.jfc";
     Options opt = new OptionsBuilder()
-            .include(".*Crc.*")
+            //.include(".*Crc.*")
             //                .include(".*Reflections.*")
             //                .addProfiler(JmhProfiler.class)
             //                .addProfiler(CompilerProfiler.class)
