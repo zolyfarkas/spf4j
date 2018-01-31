ee8838e8a7183e5798136ab0e06808843ad238c9
Zoltan Farkas <zolyfarkas@yahoo.com>
2018-01-31 18:54:47 -0500

    [add] add a JUnit runner

---
+++ spf4j-slf4j-test/src/main/java/org/spf4j/test/log/junit/Spf4jTestLogJUnitRunner.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2018 SPF4J.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.spf4j.test.log.junit;
+
+import org.junit.runner.notification.RunNotifier;
+import org.junit.runners.BlockJUnit4ClassRunner;
+import org.junit.runners.model.InitializationError;
+
+/**
+ * @author Zoltan Farkas
+ */
+public class Spf4jTestLogJUnitRunner extends BlockJUnit4ClassRunner {
+
+  public Spf4jTestLogJUnitRunner(final Class<?> klass) throws InitializationError {
+    super(klass);
+  }
+
+  /**
+   * See JUnit doc if overwriting...
+   * @param notifier
+   */
+  @Override
+  public void run(final RunNotifier notifier) {
+    Spf4jTestLogRunListenerSingleton listener = Spf4jTestLogRunListenerSingleton.getInstance();
+    notifier.removeListener(listener);
+    notifier.addListener(listener);
+    super.run(notifier);
+  }
+
+}
