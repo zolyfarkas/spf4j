2539081b1885e5f76ada328bf6e502c2f3593459
Zoltan Farkas <zolyfarkas@yahoo.com>
2018-03-16 14:44:35 -0400

    [add] reduce flakyness

--- spf4j-core/src/test/java/org/spf4j/concurrent/LifoThreadPoolExecutorCoreIdlingTest.java
+++ spf4j-core/src/test/java/org/spf4j/concurrent/LifoThreadPoolExecutorCoreIdlingTest.java
@@ -90,7 +90,7 @@ public class LifoThreadPoolExecutorCoreIdlingTest {
     LOG.info("Cpu profile saved to {}", s.dumpToFile());
     LOG.debug("CPU time = {} ns", cpuTime);
     s.stop();
-    Assert.assertTrue("CPU Time = " + cpuTime, cpuTime < 1000000000); // 6069497000 with bug  53945000 without bug
+    Assert.assertTrue("CPU Time = " + cpuTime, cpuTime < 1500000000); // 6069497000 with bug  53945000 without bug
     executor.shutdown();
     executor.awaitTermination(1, TimeUnit.SECONDS);
   }
