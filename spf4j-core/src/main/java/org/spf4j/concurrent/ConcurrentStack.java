cc7d0660858202d0706ceef262ce4dfa469c4bc0
Zoltan Farkas <zolyfarkas@yahoo.com>
2018-04-20 13:36:22 -0400

    [add] a simple concurrent stack

---
+++ spf4j-core/src/main/java/org/spf4j/concurrent/ConcurrentStack.java
@@ -0,0 +1,87 @@
+/*
+ * Copyright (c) 2001-2017, Zoltan Farkas All Rights Reserved.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * Additionally licensed with:
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.spf4j.concurrent;
+
+import java.util.concurrent.atomic.AtomicReference;
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import javax.annotation.concurrent.ThreadSafe;
+
+/**
+ * A Treiber stack.
+ * @author Zoltan Farkas
+ * @param <E>
+ */
+@ThreadSafe
+public final class ConcurrentStack<E> {
+
+  private final AtomicReference<Node<E>> top = new AtomicReference<Node<E>>();
+
+  public void push(@Nonnull final E item) {
+    Node<E> newHead = new Node<E>(item);
+    Node<E> oldHead;
+    do {
+      oldHead = top.get();
+      newHead.next = oldHead;
+    } while (!top.compareAndSet(oldHead, newHead));
+  }
+
+  @Nullable
+  public E pop() {
+    Node<E> oldHead;
+    Node<E> newHead;
+    do {
+      oldHead = top.get();
+      if (oldHead == null) {
+        return null;
+      }
+      newHead = oldHead.next;
+    } while (!top.compareAndSet(oldHead, newHead));
+    return oldHead.item;
+  }
+
+  private static class Node<E> {
+
+    private final E item;
+    private Node<E> next;
+
+    Node(final E item) {
+      this.item = item;
+    }
+  }
+
+  @Override
+  public String toString() {
+    return "ConcurrentStack{" + "top=" + top + '}';
+  }
+
+}
