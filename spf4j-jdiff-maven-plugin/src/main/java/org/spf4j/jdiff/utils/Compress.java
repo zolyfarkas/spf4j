7644826ba0f5a7fe388de7622444c4f6d8f2c21c
Zoltan Farkas <zolyfarkas@yahoo.com>
2018-06-04 09:24:17 -0400

    [fix] potential resource leak.

--- spf4j-jdiff-maven-plugin/src/main/java/org/spf4j/jdiff/utils/Compress.java
+++ spf4j-jdiff-maven-plugin/src/main/java/org/spf4j/jdiff/utils/Compress.java
@@ -51,13 +51,13 @@ import java.nio.file.attribute.BasicFileAttributes;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Stream;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
 /**
  * @author zoly
  */
-
 public final class Compress {
 
   private Compress() {
@@ -65,6 +65,7 @@ public final class Compress {
 
   /**
    * Zip a file or folder.
+   *
    * @param fileOrFolderToCompress file or folder to compress.
    * @return the Path of the compressed file. It will created in the same folder as the input parent.
    * @throws IOException
@@ -79,9 +80,9 @@ public final class Compress {
     return destFile;
   }
 
-
   /**
    * Zip a file or folder.
+   *
    * @param fileOrFolderToCompress file or folder to compress.
    * @param destFile the destination zip file.
    * @throws IOException
@@ -102,7 +103,8 @@ public final class Compress {
       }
       try (BufferedOutputStream fos = new BufferedOutputStream(Files.newOutputStream(tmpFile));
               ZipOutputStream zos = new ZipOutputStream(fos, StandardCharsets.UTF_8)) {
-          Files.walk(fileOrFolderToCompress).forEach((path) -> {
+        try (Stream<Path> stream = Files.walk(fileOrFolderToCompress)) {
+          stream.forEach((path) -> {
             if (Files.isDirectory(path)) {
               return;
             }
@@ -115,6 +117,7 @@ public final class Compress {
               throw new UncheckedIOException("Error compressing " + path, ex);
             }
           });
+        }
       }
       Files.move(tmpFile, destFile,
               StandardCopyOption.ATOMIC_MOVE, StandardCopyOption.REPLACE_EXISTING);
@@ -124,8 +127,9 @@ public final class Compress {
   }
 
   /**
-   * Copy file atomic.
-   * file will be written to a tmp file in the destination folder, and atomically renamed (if file system supports)
+   * Copy file atomic. file will be written to a tmp file in the destination folder, and atomically renamed (if file
+   * system supports)
+   *
    * @param source
    * @param destinationFile
    * @throws IOException
@@ -149,9 +153,9 @@ public final class Compress {
     }
   }
 
-
   /**
    * Unzip a zip archive to same folder.
+   *
    * @param zipFile
    * @return list of unzipped files.
    * @throws IOException
@@ -166,6 +170,7 @@ public final class Compress {
 
   /**
    * Unzip a zip file to a destination folder.
+   *
    * @param zipFile
    * @param destinationDirectory
    * @return the list of files that were extracted.
@@ -182,7 +187,7 @@ public final class Compress {
     URI zipUri = URI.create("jar:" + zipFile.toUri().toURL());
     try (FileSystem zipFs = FileSystems.newFileSystem(zipUri, Collections.emptyMap())) {
       for (Path root : zipFs.getRootDirectories()) {
-        Path dest =  destinationDirectory.resolve(root.toString().substring(1));
+        Path dest = destinationDirectory.resolve(root.toString().substring(1));
         Files.walkFileTree(root, new SimpleFileVisitor<Path>() {
           @Override
           public FileVisitResult visitFile(final Path file, final BasicFileAttributes attrs)
@@ -216,70 +221,69 @@ public final class Compress {
     return response;
   }
 
-   public static long copy(final InputStream is, final OutputStream os) throws IOException {
-        return copy(is, os, 8192);
-    }
+  public static long copy(final InputStream is, final OutputStream os) throws IOException {
+    return copy(is, os, 8192);
+  }
 
-    /**
-     * Equivalent to guava ByteStreams.copy, with one special behavior: if is has no bytes immediately available for
-     * read, the os is flushed prior to the next read that will probably block.
-     *
-     * I believe this behavior will yield better performance in most scenarios. This method also makes use of:
-     * Arrays.getBytesTmp. THis method should not be invoked from any context making use of Arrays.getBytesTmp.
-     *
-     * @param is
-     * @param os
-     * @param buffSize
-     * @throws IOException
-     */
-    public static long copy(final InputStream is, final OutputStream os, final int buffSize) throws IOException {
-        if (buffSize < 2) {
-            int val;
-            long count = 0;
-            while ((val = is.read()) >= 0) {
-                os.write(val);
-                count++;
-            }
-            return count;
+  /**
+   * Equivalent to guava ByteStreams.copy, with one special behavior: if is has no bytes immediately available for read,
+   * the os is flushed prior to the next read that will probably block.
+   *
+   * I believe this behavior will yield better performance in most scenarios. This method also makes use of:
+   * Arrays.getBytesTmp. THis method should not be invoked from any context making use of Arrays.getBytesTmp.
+   *
+   * @param is
+   * @param os
+   * @param buffSize
+   * @throws IOException
+   */
+  public static long copy(final InputStream is, final OutputStream os, final int buffSize) throws IOException {
+    if (buffSize < 2) {
+      int val;
+      long count = 0;
+      while ((val = is.read()) >= 0) {
+        os.write(val);
+        count++;
+      }
+      return count;
+    }
+    long total = 0;
+    byte[] buffer = new byte[buffSize];
+    boolean done = false;
+    long bytesCopiedSinceLastFlush = 0;
+    while (!done) {
+      // non-blocking(if input is implemented correctly) read+write as long as data is available.
+      while (is.available() > 0) {
+        int nrRead = is.read(buffer, 0, buffSize);
+        if (nrRead < 0) {
+          done = true;
+          break;
+        } else {
+          os.write(buffer, 0, nrRead);
+          total += nrRead;
+          bytesCopiedSinceLastFlush += nrRead;
         }
-        long total = 0;
-        byte[] buffer = new byte[buffSize];
-            boolean done = false;
-            long bytesCopiedSinceLastFlush = 0;
-            while (!done) {
-                // non-blocking(if input is implemented correctly) read+write as long as data is available.
-                while (is.available() > 0) {
-                    int nrRead = is.read(buffer, 0, buffSize);
-                    if (nrRead < 0) {
-                        done = true;
-                        break;
-                    } else {
-                        os.write(buffer, 0, nrRead);
-                        total += nrRead;
-                        bytesCopiedSinceLastFlush += nrRead;
-                    }
-                }
-            // there seems to be nothing available to read anymore,
-                // lets flush the os instead of blocking for another read.
-                if (bytesCopiedSinceLastFlush > 0) {
-                    os.flush();
-                    bytesCopiedSinceLastFlush = 0;
-                }
-                if (done) {
-                    break;
-                }
-                // most likely a blocking read.
-                int nrRead = is.read(buffer, 0, buffSize);
-                if (nrRead < 0) {
-                    break;
-                } else {
-                    os.write(buffer, 0, nrRead);
-                    total += nrRead;
-                    bytesCopiedSinceLastFlush += nrRead;
-                }
-            }
-        return total;
+      }
+      // there seems to be nothing available to read anymore,
+      // lets flush the os instead of blocking for another read.
+      if (bytesCopiedSinceLastFlush > 0) {
+        os.flush();
+        bytesCopiedSinceLastFlush = 0;
+      }
+      if (done) {
+        break;
+      }
+      // most likely a blocking read.
+      int nrRead = is.read(buffer, 0, buffSize);
+      if (nrRead < 0) {
+        break;
+      } else {
+        os.write(buffer, 0, nrRead);
+        total += nrRead;
+        bytesCopiedSinceLastFlush += nrRead;
+      }
     }
-
+    return total;
+  }
 
 }
