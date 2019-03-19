46ebab9429c2728aa91b4e8ea5c2e0b2ea3533de
Zoltan Farkas <zolyfarkas@yahoo.com>
2019-03-19 14:22:15 -0400

    [add] upgrade schema.

--- spf4j-zel/src/test/java/org/spf4j/zel/vm/CondDerefTest.java
+++ spf4j-zel/src/test/java/org/spf4j/zel/vm/CondDerefTest.java
@@ -32,6 +32,11 @@
 package org.spf4j.zel.vm;
 
 import java.util.concurrent.ExecutionException;
+import java.util.function.Predicate;
+import org.apache.avro.Schema;
+import org.apache.avro.SchemaBuilder;
+import org.apache.avro.generic.GenericData;
+import org.apache.avro.generic.GenericRecord;
 import org.junit.Assert;
 import org.junit.Test;
 import org.slf4j.Logger;
@@ -73,5 +78,16 @@ public final class CondDerefTest {
     Program prog = Program.compile("a.url", "a");
     LOG.debug("Program = {}", prog);
     prog.execute((Object) null);
+  }
+
+
+  @Test
+  public void test5() throws CompileException, ExecutionException, InterruptedException {
+    Predicate<GenericRecord> p = Program.compilePredicate("a?.url==null", "a");
+    Schema schema = SchemaBuilder.builder().record("test").fields()
+            .optionalString("url").endRecord();
+    GenericData.Record record = new GenericData.Record(schema);
+    Assert.assertTrue(p.test(record));
+
   }
 }
