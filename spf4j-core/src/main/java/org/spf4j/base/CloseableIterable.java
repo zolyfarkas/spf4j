f751bbe65f894fc182d930cce356548ed616ae88
Zoltan Farkas <zolyfarkas@yahoo.com>
2019-10-14 12:06:37 -0400

    [refactor] move CloseableIterable to base.

---
+++ spf4j-core/src/main/java/org/spf4j/base/CloseableIterable.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2019 SPF4J.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.spf4j.base;
+
+import edu.umd.cs.findbugs.annotations.CleanupObligation;
+import java.io.Closeable;
+import java.util.Iterator;
+
+/**
+ * @author Zoltan Farkas
+ */
+@CleanupObligation
+public interface CloseableIterable<T> extends Closeable, Iterable<T> {
+
+
+  void close();
+
+  static <T> CloseableIterable<T> fromIterable(final Iterable<T> it) {
+    return new CloseableIterable<T>() {
+      @Override
+      public void close() {
+        // nothing to close;
+      }
+
+      @Override
+      public Iterator<T> iterator() {
+        return it.iterator();
+      }
+    };
+  }
+
+}
