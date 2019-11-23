afd937573d193a3725e5895cbace7a8d347305c1
Zoltan Farkas <zolyfarkas@yahoo.com>
2019-11-23 09:59:42 -0500

    [optimize] and make more flexible

--- spf4j-core/src/main/java/org/spf4j/io/csv/CharSeparatedValues.java
+++ spf4j-core/src/main/java/org/spf4j/io/csv/CharSeparatedValues.java
@@ -38,7 +38,6 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.Reader;
-import java.io.StringWriter;
 import java.io.UncheckedIOException;
 import java.io.Writer;
 import java.nio.charset.Charset;
