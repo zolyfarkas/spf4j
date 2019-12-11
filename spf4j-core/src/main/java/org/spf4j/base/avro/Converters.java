31013e2234da8b1ae449f3d7cb50f79ff4e964d7
Zoltan Farkas <zolyfarkas@yahoo.com>
2019-12-11 07:41:53 -0500

    [cleanup] root name meaningless in distributed env.

--- spf4j-core/src/main/java/org/spf4j/base/avro/Converters.java
+++ spf4j-core/src/main/java/org/spf4j/base/avro/Converters.java
@@ -32,7 +32,6 @@
 package org.spf4j.base.avro;
 
 import gnu.trove.map.TMap;
-import java.lang.management.ManagementFactory;
 import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -53,7 +52,7 @@ import org.spf4j.log.Slf4jLogRecord;
 @ParametersAreNonnullByDefault
 public final class Converters {
 
-  public static final Method ROOT = new Method(ManagementFactory.getRuntimeMXBean().getName(), "ROOT");
+  public static final Method ROOT = new Method("JVM", "ROOT");
 
   private Converters() { }
 
