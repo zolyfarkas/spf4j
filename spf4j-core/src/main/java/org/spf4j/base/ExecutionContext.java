e10b90ab544982698343106bede9f4e7a64c76e7
Zoltan Farkas <zolyfarkas@yahoo.com>
2020-01-17 10:58:49 -0500

    [cleanup]

--- spf4j-core/src/main/java/org/spf4j/base/ExecutionContext.java
+++ spf4j-core/src/main/java/org/spf4j/base/ExecutionContext.java
@@ -107,32 +107,6 @@ public interface ExecutionContext extends AutoCloseable, JsonWriteable {
   @DischargesObligation
   void close();
 
-  default void closeAll() {
-    close();
-    ExecutionContext current = this;
-    ExecutionContext notClosedParent;
-    while ((notClosedParent = current.getNotClosedParent()) != null) {
-      notClosedParent.close();
-      current = notClosedParent;
-    }
-  }
-
-  default void closeAllButRoot() {
-    if (this.getSource() == null) {
-      return;
-    }
-    close();
-    ExecutionContext current = this;
-    ExecutionContext notClosedParent;
-    while ((notClosedParent = current.getNotClosedParent()) != null) {
-      if (notClosedParent.getSource() == null) {
-        return;
-      }
-      notClosedParent.close();
-      current = notClosedParent;
-    }
-  }
-
   @Nonnull
   String getName();
 
