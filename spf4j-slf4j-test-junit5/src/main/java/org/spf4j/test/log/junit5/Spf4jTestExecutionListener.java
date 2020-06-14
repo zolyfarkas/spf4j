5b59a8b1879529318c9aced957f1b08cdf97caac
Zoltan Farkas <zolyfarkas@yahoo.com>
2020-06-14 07:37:40 -0400

    [fix]no param tests

--- spf4j-slf4j-test-junit5/src/main/java/org/spf4j/test/log/junit5/Spf4jTestExecutionListener.java
+++ spf4j-slf4j-test-junit5/src/main/java/org/spf4j/test/log/junit5/Spf4jTestExecutionListener.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package orrg.spf4j.test.log.junit5;
+package org.spf4j.test.log.junit5;
 
 import java.lang.annotation.Annotation;
 import java.util.List;
