34facc213089f8b55001f63099f4b2667a34ffbc
Zoltan Farkas <zolyfarkas@yahoo.com>
2021-04-17 00:06:34 -0400

    [add] remove deprecation

--- spf4j-core/src/main/java/org/spf4j/base/LookupTranslator.java
+++ spf4j-core/src/main/java/org/spf4j/base/LookupTranslator.java
@@ -38,9 +38,9 @@ package org.spf4j.base;
  * The ASF licenses this file to You under the Apache License, Version 2.0
  * (the "License"); you may not use this file except in compliance with
  * the License.  You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -48,6 +48,7 @@ package org.spf4j.base;
  * limitations under the License.
  */
 
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import java.io.IOException;
 import java.io.Writer;
 import java.util.HashMap;
@@ -59,7 +60,7 @@ import java.util.Objects;
  * @since 3.0
  * @version $Id$
  */
-@edu.umd.cs.findbugs.annotations.SuppressWarnings("CLI_CONSTANT_LIST_INDEX")
+@SuppressFBWarnings("CLI_CONSTANT_LIST_INDEX")
 public final class LookupTranslator extends CharSequenceTranslator {
 
     private final HashMap<String, CharSequence> lookupMap;
@@ -143,10 +144,6 @@ public final class LookupTranslator extends CharSequenceTranslator {
         }
         return Objects.equals(this.lookupMap, other.lookupMap);
     }
-
-
-
-
 
 }
 
