912e64726a284196ece42b1b078b162586985fc5
Zoltan Farkas <zolyfarkas@yahoo.com>
2021-08-27 11:19:41 -0400

    [add] utility supplier for character arrays.

---
+++ spf4j-core/src/main/java/org/spf4j/base/CharArrayConsumer.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright 2021 SPF4J.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.spf4j.base;
+
+/**
+ * @author Zoltan Farkas
+ */
+public interface CharArrayConsumer {
+  void accept(char[] array, int offset, int length);
+}
